3acf4c5b24b53c2ee2b929a60030245fd2d800a3
Kiwijang <kiwijang5473@gmail.com>
2018-09-14 21:09:00 +0800

    0914_w1_hw3 上傳

--- homeworks/week1/hw3.js
+++ homeworks/week1/hw3.js
@@ -1,3 +1,12 @@
 export const isPrime = (n) => {
-  return false;
+  for(var i=2; i<=n; i++){
+    if(n%i===0){
+      var mynum=i
+      if(mynum<n){
+        return false
+      }else{
+        return true
+      }
+    }
+  }
 }
