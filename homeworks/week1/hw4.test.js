e55ea3654f87823a738d03779199548aef70e7dd
Kiwijang <kiwijang5473@gmail.com>
2018-09-14 22:12:49 +0800

    0914_w1_hw4

--- homeworks/week1/hw4.test.js
+++ homeworks/week1/hw4.test.js
@@ -1,7 +1,16 @@
 import {isPalindromes} from './hw4'
 
 describe("hw4", () => {
-  it("should return correct answer when str = abcdcba", () => {
-    expect(isPalindromes('abcdcba')).toBe(true)
+  it("should return correct answer when str = abcba", () => {
+    expect(isPalindromes('abcba')).toBe(true)
+  })
+  it("should return correct answer when str = abcba",()=>{
+    expect(isPalindromes('apple')).toBe(false)
+  })
+  it("should return correct answer when str = apple",()=>{
+    expect(isPalindromes('aaaaa')).toBe(true)
+  })
+  it("should return correct answer when str = apple",()=>{
+    expect(isPalindromes('applppa')).toBe(true)
   })
 })
